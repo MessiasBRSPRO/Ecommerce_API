ac76b53fd1231a0aff0b44938881cdde3dfda905
unknown <meddisme@gmail.com>
2025-08-08 09:18:13 -0300

    creating Product Entity

---
+++ src/main/java/com/Project/Ecommerce/Entities/Product.java
@@ -0,0 +1,19 @@
+package com.Project.Ecommerce.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+
+    private Double price;
+
+    private Integer stock;
+
+}
