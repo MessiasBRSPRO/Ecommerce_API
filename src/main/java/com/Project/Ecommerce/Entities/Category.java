2eb7dc6f39337b88ccad89f7d56659001503fc18
unknown <meddisme@gmail.com>
2025-08-08 09:28:47 -0300

    Creating category entity

---
+++ src/main/java/com/Project/Ecommerce/Entities/Category.java
@@ -0,0 +1,16 @@
+package com.Project.Ecommerce.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+
+}
