cbc4e44ca2459067f5284034fda8a6e4e7022346
massiasBRSPRO <meddisme@gmail.com>
2025-08-19 13:57:07 -0300

    Structuring the Category entity

--- src/main/java/com/Project/Ecommerce/Entities/Category.java
+++ src/main/java/com/Project/Ecommerce/Entities/Category.java
@@ -1,6 +1,10 @@
 package com.Project.Ecommerce.Entities;
 
+import com.Project.Ecommerce.DTOs.CategoryDTO;
 import jakarta.persistence.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 @Entity
 @Table(name = "categories")
@@ -12,5 +16,41 @@ public class Category {
 
     private String categoryName;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private ArrayList<Product> products;
 
+    public Category(){}
+
+    public Category(Long id, String categoryName){
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    public Category(CategoryDTO categoryDTO){
+        this.id = categoryDTO.id();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
 }
