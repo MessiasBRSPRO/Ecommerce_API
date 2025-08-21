5b2becc69abadb41a43c8ebdbe90c59aa35e15fb
massiasBRSPRO <meddisme@gmail.com>
2025-08-21 09:12:04 -0300

    adding new attribute category in DTOListedProduct record

--- src/main/java/com/Project/Ecommerce/DTOs/DTOListedProduct.java
+++ src/main/java/com/Project/Ecommerce/DTOs/DTOListedProduct.java
@@ -1,10 +1,11 @@
 package com.Project.Ecommerce.DTOs;
 
+import com.Project.Ecommerce.Entities.Category;
 import com.Project.Ecommerce.Entities.Product;
 
-public record DTOListedProduct(Long id, String productName, Double price) {
+public record DTOListedProduct(Long id, String productName, Double price, Category category) {
 
     public DTOListedProduct(Product product){
-        this(product.getId(), product.getProductName(), product.getPrice());
+        this(product.getId(), product.getProductName(), product.getPrice(), product.getCategory());
     }
 }
