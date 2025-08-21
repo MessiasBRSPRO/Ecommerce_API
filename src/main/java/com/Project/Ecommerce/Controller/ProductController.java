98f5ab0e230c9eba30a16dd291aee5c0e1cfccb2
massiasBRSPRO <meddisme@gmail.com>
2025-08-21 09:07:33 -0300

    Creating method search products list by id

--- src/main/java/com/Project/Ecommerce/Controller/ProductController.java
+++ src/main/java/com/Project/Ecommerce/Controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -35,5 +34,11 @@ public class ProductController {
     @RequestMapping("/all-products")
     public ResponseEntity<List<DTOListedProduct>> allProductsEndpoint(){
         return ResponseEntity.ok(productService.allProducts());
+    }
+
+    @GetMapping
+    @RequestMapping("/all-products/{id}")
+    public ResponseEntity<List<DTOListedProduct>> allProductsEndpoint(@PathVariable Long id){
+        return ResponseEntity.ok().body(productService.allProductsById(id));
     }
 }
--- src/main/java/com/Project/Ecommerce/Services/ProductService.java
+++ src/main/java/com/Project/Ecommerce/Services/ProductService.java
@@ -1,13 +1,14 @@
 package com.Project.Ecommerce.Services;
 
+import com.Project.Ecommerce.DTOs.CategoryDTO;
 import com.Project.Ecommerce.DTOs.DTOListedProduct;
 import com.Project.Ecommerce.DTOs.ProductDTO;
+import com.Project.Ecommerce.Entities.Category;
 import com.Project.Ecommerce.Entities.Product;
 import com.Project.Ecommerce.Repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -33,5 +34,17 @@ public class ProductService {
 
     public List<DTOListedProduct> allProducts(){
         return productRepository.findAll().stream().map(DTOListedProduct::new).toList();
+    }
+
+    public List<DTOListedProduct> allProductsById(Long id){
+        List<DTOListedProduct> productById = new ArrayList<>();
+        Category category = new Category(new CategoryDTO(id));
+        for (DTOListedProduct product : allProducts()){
+            if(product.category().equals(category)){
+                productById.add(product);
+            }
+        }
+        return productById;
+
     }
 }
